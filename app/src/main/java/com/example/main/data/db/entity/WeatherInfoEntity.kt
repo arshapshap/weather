8789475167d2bbec159e57239421c059166271d6
Artyom Shapovalov <arshapshap@gmail.com>
2023-05-18 11:02:15 +0300

    fix weatherInfoEntity

--- app/src/main/java/com/example/main/data/db/entity/WeatherInfoEntity.kt
+++ app/src/main/java/com/example/main/data/db/entity/WeatherInfoEntity.kt
@@ -6,13 +6,13 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "Weather")
 data class WeatherInfoEntity(
-    @ColumnInfo(name = "city_name") @PrimaryKey var cityName: String,
-    @ColumnInfo(name = "temperature_in_celsius") var temperatureInCelsius: Int,
-    @ColumnInfo(name = "humidity_as_percentage") var humidityAsPercentage: Int,
-    @ColumnInfo(name = "pressure_in_millimeters_of_mercury") var pressureInMillimetersOfMercury: Int,
-    @ColumnInfo(name = "wind_speed_in_meters_per_second") var windSpeedInMetersPerSecond: Double,
-    @ColumnInfo(name = "image_url") var imageUrl: String,
-    @ColumnInfo(name = "latitude") var latitude: Double?,
-    @ColumnInfo(name = "longitude") var longitude: Double?,
-    @ColumnInfo(name = "last_update") var lastUpdate: Long,
+    @ColumnInfo(name = "city_name") @PrimaryKey val cityName: String,
+    @ColumnInfo(name = "temperature_in_celsius") val temperatureInCelsius: Int,
+    @ColumnInfo(name = "humidity_as_percentage") val humidityAsPercentage: Int,
+    @ColumnInfo(name = "pressure_in_millimeters_of_mercury") val pressureInMillimetersOfMercury: Int,
+    @ColumnInfo(name = "wind_speed_in_meters_per_second") val windSpeedInMetersPerSecond: Double,
+    @ColumnInfo(name = "image_url") val imageUrl: String,
+    @ColumnInfo(name = "latitude") val latitude: Double?,
+    @ColumnInfo(name = "longitude") val longitude: Double?,
+    @ColumnInfo(name = "last_update") val lastUpdate: Long,
 )
